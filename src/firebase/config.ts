43ad3dd75c50ee6edd31be6b58afe85ade0299b2
Omar Lopez <omar850413@gmail.com>
2025-11-10 23:17:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5032321412-19de7",
+  "appId": "1:340261769953:web:dded14f849ca05d6399da0",
+  "apiKey": "AIzaSyCVUoatlhSlxMeFSEvhnoxc67o8q1o85FM",
+  "authDomain": "studio-5032321412-19de7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "340261769953"
+};
