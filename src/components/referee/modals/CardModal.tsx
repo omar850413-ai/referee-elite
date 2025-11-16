4e6fccff05deceacd1a8b262d20ccec51c067dce
Omar Lopez <omar850413@gmail.com>
2025-11-16 23:24:38 +0000

    Puedes hacer que solo los menús desplegables donde redujiste las letras

--- src/components/referee/modals/CardModal.tsx
+++ src/components/referee/modals/CardModal.tsx
@@ -230,7 +230,7 @@ const CardModal = ({ isOpen, dispatch, modalData, timerIsRunning, teamNames }: C
               <SelectTrigger id="card-reason">
                 <SelectValue placeholder="Seleccione una causal" />
               </SelectTrigger>
-              <SelectContent>
+              <SelectContent className="reason-select-content">
                 {(cardType === 'yellow' ? yellowCardReasons : redCardReasons).map((r, index) => (
                   <SelectItem key={index} value={r}>
                     {r}
@@ -246,7 +246,7 @@ const CardModal = ({ isOpen, dispatch, modalData, timerIsRunning, teamNames }: C
                 <SelectTrigger id="card-sub-reason">
                   <SelectValue placeholder="Seleccione un criterio" />
                 </SelectTrigger>
-                <SelectContent>
+                <SelectContent className="reason-select-content">
                   {unsportingBehaviorReasons.map((subReason, index) => (
                     <SelectItem key={index} value={subReason}>
                       {subReason}
