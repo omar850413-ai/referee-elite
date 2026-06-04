1f46619aa18ebd4aec0c2229f3d14a5e909efe52
omar850413-ai <omar850413@gmail.com>
2026-06-03 20:34:02 -0600

    Add files via upload

---
+++ config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBztOsIGGaiQSgn31ESPeRS56sfVbbsEz4",
+  authDomain: "referee-elite-eaf2e.firebaseapp.com",
+  projectId: "referee-elite-eaf2e",
+  storageBucket: "referee-elite-eaf2e.firebasestorage.app",
+  messagingSenderId: "206115613222",
+  appId: "1:206115613222:web:41cc80a12c983a596214e7"
+};
