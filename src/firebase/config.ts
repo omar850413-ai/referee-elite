d1798119e11cc9c4e4a0fd168613d0fcf9616a7a
omar850413-ai <omar850413@gmail.com>
2026-06-03 22:22:25 -0600

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBPeer0wfb5pbyXJvWwBwXkjdJN2LbNQP0",
-  authDomain: "soy-docente-58544610-7d370.firebaseapp.com",
-  databaseURL: "https://soy-docente-58544610-7d370-default-rtdb.firebaseio.com",
-  projectId: "soy-docente-58544610-7d370",
-  storageBucket: "soy-docente-58544610-7d370.firebasestorage.app",
-  messagingSenderId: "978987146644",
-  appId: "1:978987146644:web:4159d13276ab7178c2c381"
+  apiKey: "AIzaSyBztOsIGGaiQSgn31ESPeRS56sfVbbsEz4",
+  authDomain: "referee-elite-eaf2e.firebaseapp.com",
+  projectId: "referee-elite-eaf2e",
+  storageBucket: "referee-elite-eaf2e.firebasestorage.app",
+  messagingSenderId: "206115613222",
+  appId: "1:206115613222:web:41cc80a12c983a596214e7"
 };
